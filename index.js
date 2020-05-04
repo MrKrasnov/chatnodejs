ebafef7aaf4751a7becb8ccdddb04c5b72da7ef2
unknown <mr.krasnov.y@gmail.com>
2020-05-04 17:24:40 +0300

    Connected at mongodb

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
-
+const mongodb = require('mongodb');
 const app = express()
+
+// routes
 
 app.get('/', function (req, res) {
     res.send('root layer');
 })
 
-app.listen(8080, function() {
-    console.log('server listening at port :8080');
+app.get('/registration', function (req, res) {
+    res.send('layer registration');
 })
+
+//подключение к mongoDB
+async function start() {
+    try{
+    const client = await mongodb.MongoClient.connect
+    ('mongodb://localhost:27017/cash-control', {
+        useNewUrlParser: true,
+        userFindAndModify: false,
+    })
+    app.listen(8080, function() {
+        console.log('server listening at port :8080');
+    })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start()
+
